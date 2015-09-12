cdbae5532de57bea43b1d6727366765adbca47ca
Boxin Ma <bma7@studentmail.nait.ca>
2015-09-11 20:53:55 -0600

    initial project setup

---
+++ repowebsite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(repowebsite.Startup))]
+namespace repowebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
